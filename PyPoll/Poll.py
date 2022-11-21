b0f872aa20d2a61ef7a72a50bbd6300309c07f67
Haiming Wang <haimingwang@Haimings-MBP.hitronhub.home>
2022-11-20 21:34:34 -0500

    I updated files for resubmission

--- PyPoll/Poll.py
+++ PyPoll/Poll.py
@@ -17,34 +17,42 @@ with open(csv_path, 'r') as election_data:
 Total_votes = len(election_data_list)
 print("Total Votes:", Total_votes)
 
-row_count_Charles = 0
-row_count_Diana = 0
-row_count_Raymon = 0
+row_count_1 = 0
+row_count_2 = 0
+row_count_3 = 0
 
 for row in election_data_list:
-  if row[2] == "Charles Casper Stockham":
-    row_count_Charles += 1
-  elif row[2] == "Diana DeGette":
-    row_count_Diana += 1
-  elif row[2] == "Raymon Anthony Doane":
-    row_count_Raymon += 1
+  candidate1 = "Charles Casper Stockham"
+  candidate2 = "Diana DeGette"
+  candidate3 = "Raymon Anthony Doane"
+  if row[2] == candidate1:
+    row_count_1 += 1
+  elif row[2] == candidate2:
+    row_count_2 += 1
+  elif row[2] == candidate3:
+    row_count_3 += 1
     
-print("Charles Casper Stockham:", row_count_Charles/Total_votes*100,"% (",row_count_Charles,")")
-print("Diana DeGette:", row_count_Diana/Total_votes*100,"% (",row_count_Diana,")")
-print("Raymon Anthony Doane:", row_count_Raymon/Total_votes*100,"% (",row_count_Raymon,")")
+print("Charles Casper Stockham:", row_count_1/Total_votes*100,"% (",row_count_1,")")
+print("Diana DeGette:", row_count_2/Total_votes*100,"% (",row_count_2,")")
+print("Raymon Anthony Doane:", row_count_3/Total_votes*100,"% (",row_count_3,")")
 print("------------------------------------")
 
-if row_count_Charles > row_count_Diana and row_count_Charles > row_count_Raymon:
-  print("Winner: Charles Casper Stockham", )
-elif row_count_Raymon > row_count_Charles and row_count_Raymon > row_count_Diana:
-  print("Winner: Raymon Anthony Doane")
-elif row_count_Diana > row_count_Charles and row_count_Diana > row_count_Raymon:
-  print("Winner: Diana DeGette")
+if row_count_1 > row_count_2 and row_count_1 > row_count_3:
+  print("Winner:" + candidate1 )
+elif row_count_3 > row_count_1 and row_count_3 > row_count_2:
+  print("Winner:" + candidate3)
+elif row_count_2 > row_count_1 and row_count_2 > row_count_3:
+  print("Winner:" + candidate2)
 
-# vote_counts = [row_count_Charles, row_count_Diana, row_count_Raymon]
-# winners_name = max(vote_counts)
-# print(winners_name)
-
+with open('Analysis_Result_Pypoll.txt','w') as text:
+    text.write('Election Results\n')
+    text.write('-------------------------\n')
+    text.write(f"Total Votes: {Total_votes}\n")
+    text.write(f"Charles Casper Stockham: {row_count_1/Total_votes*100}% ({row_count_1})\n")
+    text.write(f"Diana DeGette: {row_count_2/Total_votes*100}% ({row_count_2})\n")
+    text.write(f"Raymond Anthony Doane: {row_count_3/Total_votes*100}% ({row_count_3})\n")
+    text.write('-------------------------\n')
+    text.write(f"Winner: {candidate2}")
 
 
 
